a528f6e6f89200b70ecdb0842675d283600b67e3
Garrett Wong <wong_ga@sc.wdc.com>
2016-10-19 00:17:47 -0700

    initial commit of azure web storage

---
+++ AzureWebStorage/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureWebStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AzureWebStorageConnection awsc = new AzureWebStorageConnection();
+
+            awsc.List();
+
+            awsc.CreateContainer("testcontainer");
+
+            awsc.AddFromFile("testcontainer", @"C:\data\test.txt");
+
+            awsc.List();
+
+            awsc.GetString("testcontainer", "asdf");
+        }
+    }
+}
